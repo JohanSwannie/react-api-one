c522aacd732ad3f6e72baff87a3dcf9e10a60ead
Johan Swan <jswannieman@gmail.com>
2023-11-08 11:57:11 +1300

    Update App JS Component

--- src/Components/App.js
+++ src/Components/App.js
@@ -29,7 +29,7 @@ class App extends Component {
           <nav>
             <div className="nav-wrapper">
               <a href="/" className="brand-logo">
-                My Feed
+                News Feed
               </a>
             </div>
           </nav>
