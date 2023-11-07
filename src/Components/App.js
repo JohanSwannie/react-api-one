d3a19b10624f5445b29062faf5d4dec56d5053f6
Johan Swan <jswannieman@gmail.com>
2023-11-08 08:27:01 +1300

    Commit App JS  Component

---
+++ src/Components/App.js
@@ -0,0 +1,15 @@
+import News from "./News";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="app-header">
+        <h1 className="app-title"></h1>
+        <News />
+      </header>
+    </div>
+  );
+}
+
+export default App;
