77efd5e77a7750d1812c74d1ce72c574d2f5413a
Johan Swan <jswannieman@gmail.com>
2023-11-08 11:57:23 +1300

    Update News JS Component

--- src/Components/News.js
+++ src/Components/News.js
@@ -1,51 +1,47 @@
-import React, { Component } from 'react';
-import NewSingle from './NewSingle';
-import Error from './Error';
+import React, { Component } from "react";
+import NewSingle from "./NewSingle";
+import Error from "./Error";
 
 class News extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            news: [],
-            error: false,
-        };
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      news: [],
+      error: false,
+    };
+  }
 
-    componentDidMount() {
-        const url = `https://newsapi.org/v2/${this.props.news.type}?${this.props.news.query}&apiKey=${process.env.REACT_APP_API}`;
+  componentDidMount() {
+    const url = `https://newsapi.org/v2/${this.props.news.type}?${this.props.news.query}&apiKey=${process.env.REACT_APP_API}`;
 
-        fetch(url)
-            .then((response) => {
-                return response.json();
-            })
-            .then((data) => {
-                this.setState({
-                    news: data.articles
-                })
-            })
-            .catch((error) => {
-                this.setState({
-                    error: true
-                })
-            });
-    }
+    fetch(url)
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        this.setState({
+          news: data.articles,
+        });
+      })
+      .catch((error) => {
+        this.setState({
+          error: true,
+        });
+      });
+  }
 
-    renderItems() {
-        if (!this.state.error) {
-            return this.state.news.map((item) => (
-                <NewSingle key={item.url} item={item}/>
-        ));
-        } else {
-            return <Error />
-        }
+  renderItems() {
+    if (!this.state.error) {
+      return this.state.news.map((item) => (
+        <NewSingle key={item.url} item={item} />
+      ));
+    } else {
+      return <Error />;
     }
-    render() {
-        return (
-            <div className='row'>
-                {this.renderItems()}
-            </div>
-        )
-    }
+  }
+  render() {
+    return <div className="row">{this.renderItems()}</div>;
+  }
 }
 
 export default News;
