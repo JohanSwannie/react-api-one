0308eab5ca896ad5c3e879e1de521c052ddb2c18
Johan Swan <jswannieman@gmail.com>
2023-11-30 08:10:26 +1300

    Update Sidenews JS Component

--- src/Components/Sidenews.js
+++ src/Components/Sidenews.js
@@ -1,49 +1,45 @@
-import React, { Component } from 'react';
-import SingleSide from './SingleSide';
-import Error from './Error';
+import React, { Component } from "react";
+import SingleSide from "./SingleSide";
+import Error from "./Error";
 
 class News extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            sidenews: [],
-            error: false,
-        };
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      sidenews: [],
+      error: false,
+    };
+  }
 
-    componentDidMount() {
-        const url = `https://newsapi.org/v2/${this.props.news.type}?${this.props.news.query}&apiKey=${process.env.REACT_APP_API}`;
+  componentDidMount() {
+    const url = `https://newsapi.org/v2/${this.props.news.type}?${this.props.news.query}&apiKey=${process.env.REACT_APP_API}`;
 
-        fetch(url)
-            .then((response) => {
-                return response.json();
-            })
-            .then((data) => {
-                this.setState({
-                    sidenews: data.articles
-                })
-            })
-            .catch((error) => {
-                console.log(error);
-            });
-    }
+    fetch(url)
+      .then((response) => {
+        return response.json();
+      })
+      .then((dataFromResponse) => {
+        this.setState({
+          sidenews: dataFromResponse.articles,
+        });
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }
 
-    renderItems() {
-        if (!this.state.error) {
-            return this.state.sidenews.map((item) => (
-                <SingleSide key={item.url} item={item}/>
-            ));
-        } else {
-            return <Error />
-        }
+  renderItems() {
+    if (!this.state.error) {
+      return this.state.sidenews.map((item) => (
+        <SingleSide key={item.url} item={item} />
+      ));
+    } else {
+      return <Error />;
     }
-    render() {
-        return (
-            <div>
-                {this.renderItems()}
-            </div>
-        )
-    }
+  }
+  render() {
+    return <div>{this.renderItems()}</div>;
+  }
 }
 
 export default News;
