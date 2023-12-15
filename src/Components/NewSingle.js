96d2aae008614bf41e6f7b703092dadfd01713e8
Johan Swan <jswannieman@gmail.com>
2023-12-16 12:22:19 +1300

    Update NewSingle JS Component

--- src/Components/NewSingle.js
+++ src/Components/NewSingle.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 const NewSingle = ({ item }) => (
-  <div className="col s4">
+  <div className="col s2">
     <div className="card medium">
       <div className="card-image">
         <img src={item.urlToImage} alt={item.title} />
