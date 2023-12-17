561290f97a66ae0f2b8858d094af9cbb8ff706a2
Johan Swan <jswannieman@gmail.com>
2023-12-18 10:35:08 +1300

    Update Error JS Component

--- src/Components/Error.js
+++ src/Components/Error.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const Error = () => (
   <div>
-    <h1>There are no news this time, Please check your sources carefully!</h1>
+    <h1>There are no news items this time, Please check your sources carefully!</h1>
   </div>
 );
 
